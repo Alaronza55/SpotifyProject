c6aa306d73ea57b92259e65b2fd6c15a9ebe230e
Alaronza55 <dalmog.dav@gmail.com>
2023-03-27 20:45:42 +0200

    test ids only

--- isDuplicate.py
+++ isDuplicate.py
@@ -34,36 +34,11 @@ for track in tracks:
     track_name = track["track"]["name"]
     artist_info = track["track"]["artists"][0]
     artist_name = artist_info['name']
-    # track_uri = track['track']["uri"]
+    track_uri = track['track']["uri"]
 
-    # print (track_name)
-    # print (track_id)
-    # print (artist_name)
-    # print (track_uri)
+    # print(track_id)
     
-    merged = '_'.join([track_id]+[track_name]+[artist_name])
+    wDuplicates = set([track_id])
 
-    # print(merged) 
-
-    # new_list = merged.split('_')
-    
-    # print(new_list)
-
-    # id = new_list[0]
-    # Name = new_list[1]
-
-    # print(id)
-    # print(Name)
-
-    # duplicates = []
-    
-    # for item in track_uri:
-    #     if track_uri.count(item) > 1 and item not in duplicates:
-    #         duplicates.append(item)
-    
-    # print(duplicates)
-
-    # sp.playlist_remove_all_occurrences_of_items
-
-
+    print(wDuplicates)
 
